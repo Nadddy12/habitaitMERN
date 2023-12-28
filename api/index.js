1702fe4ef8026c686343386ef8a0aa57c8ba12ec
Nadddy12 <abd.nad12@gmail.com>
2023-12-28 11:39:25 +0100

    connect à la DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+
+mongoose.connection.on("error", () => {
+    console.log("Erreur lors de la connexion à la base de données");
+});
+
+mongoose.connection.on("open", () => {
+    console.log("Connexion à la base de donénes établie");
+});
 
 const app = express();
 
