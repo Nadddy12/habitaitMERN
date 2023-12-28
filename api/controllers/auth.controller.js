c1bcc35f11b8c17a9f3094fe3e387f61d530d521
Nadddy12 <abd.nad12@gmail.com>
2023-12-28 12:51:44 +0100

    ajouter error handler et hash le mot de pass

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import User from './../models/user.model.js';
 
 const authController = {};
 
-authController.singup = async ( req , res ) => {
+authController.singup = async ( req , res , next) => {
     const { username , email , password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username , email , password: hashedPassword });
@@ -11,7 +11,7 @@ authController.singup = async ( req , res ) => {
         await newUser.save();
         res.status(201).json("user created successfully !")
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
 
