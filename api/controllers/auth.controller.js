4f70ce4b36b2b3b19fac88eb36e97c7332b43a0f
Nadddy12 <abd.nad12@gmail.com>
2024-01-01 15:44:39 +0100

    ajouter signin fonction

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,12 +7,24 @@ authController.singup = async ( req , res , next) => {
     const { username , email , password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username , email , password: hashedPassword });
+
     try {
         await newUser.save();
         res.status(201).json("user created successfully !")
     } catch (error) {
         next(error);
-    }
+    };
 };
+
+authController.singin = async (req , res ,next) => {
+    const {email , password} = req.body;
+    
+    try {
+        const validUser = await User.findOne({email});
+    } catch (error) {
+        next(error);
+    };
+};
+
 
 export default authController;
