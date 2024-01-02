ead1e5e8a2b47b30a71531a6d43a093593181078
Nadddy12 <abd.nad12@gmail.com>
2024-01-02 18:18:46 +0100

    ajoute Redux et crée store et reducers pour user

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart , signInFailure , signInSuccess } from '../../store/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error , setError] = useState(null);
-  const [loading , setLoading] = useState(false);
+  const { loading , error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -16,8 +18,10 @@ export default function SignIn() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try{
-      setLoading(true);
+      dispatch(signInStart());
+
       const res = await fetch(`/api/auth/signin` , {
         method: 'POST',
         headers: {
@@ -25,18 +29,17 @@ export default function SignIn() {
         },
         body: JSON.stringify(formData),
       });
+
       const data = await res.json();
       if(data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+
+      dispatch(signInSuccess(data));
       navigate(`/`)
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     };
   };
 
