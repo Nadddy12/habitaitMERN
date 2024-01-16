67b3b4ea136bd3cc483e3018863ccf69b0d53718
Nadddy12 <abd.nad12@gmail.com>
2024-01-16 12:06:55 +0100

    ajouter des pré-installations

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import listRouter from "./routes/listRouter.js"
 import authRouter from "./routes/authRouter.js"
 import userRouter from "./routes/userRouter.js"
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ mongoose.connection.on("open", () => {
     console.log("Connexion à la base de donénes établie");
 });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +28,12 @@ app.use(cookieParser());
 app.use('/api/user' , userRouter)
 app.use('/api/auth' , authRouter)
 app.use('/api/list' , listRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req , res) => {
+    res.sendFile(path.join(__dirname , 'client' , 'dist' , 'index.html'));
+});
 
 app.use((err , req , res , next) => {
     const statusCode = err.statusCode || 500;
