931d16c99156f016bf4971e6cb0a086d0a49b355
Nadddy12 <abd.nad12@gmail.com>
2024-01-16 13:17:58 +0100

    remove consolelogs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,6 @@ export default function Home() {
   const [rentList , setRentList] = useState([]);
   SwiperCore.use([Navigation]);
 
-  console.log(saleList)
 
   useEffect(() => {
     const fetchOffer = async () => {
